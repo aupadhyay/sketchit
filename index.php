29628fceeeda89d8c2711f2b7ef07fa6a0b5ddda
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2016-01-08 00:20:49 -0800

    adding some styling

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	</head>
 
 	<body>
+		<img src="sketchit.png" alt="">
 		<h1>Please upload a picture</h1>
 		<form action="upload.php" method="post" enctype="multipart/form-data">
 			<input type="file" name="fileToUpload" id="fileToUpload">
