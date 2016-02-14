75010e430227e053195b38113b9b2be80d35588f
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2016-02-13 19:30:57 -0800

    lol

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 	<body>
 		<img src="sketchit.png" alt="">
-		<h1>Please upload a picture!</h1>
+		<h1>Please upload a picture!lol</h1>
 		<form action="upload.php" method="post" enctype="multipart/form-data">
 			<input type="file" name="fileToUpload" id="fileToUpload">
     		<input type="submit" value="Upload" name="submit">
